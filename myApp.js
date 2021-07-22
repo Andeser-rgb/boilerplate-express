bac9c81a8737c639832ea469528db589fa1b45ff
Andeser-rgb <andeser@protonmail.com>
2021-07-22 19:54:44 +0200

    Added message stylization

--- myApp.js
+++ myApp.js
@@ -7,7 +7,11 @@ app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname +  "/views/index.html"));
-app.get("/json", (req, res) => res.json({message: "Hello json"}));
+app.get("/json", (req, res) => {
+    if(process.env.MESSAGE_STYLE === "uppercase")
+        return res.json({message: "HELLO JSON"})
+    return res.json({message: "Hello json"})
+});
 
 
 
