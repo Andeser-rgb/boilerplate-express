3dd89b71e5dbb79a5cd218c3a5ef3f3859f3fff6
Andeser-rgb <andeser@protonmail.com>
2021-07-22 15:58:48 +0200

    Fixed an error

--- myApp.js
+++ myApp.js
@@ -3,9 +3,9 @@ var app = express();
 
 console.log('Hello World');
 
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname +  "/views/index.html"));
-app.use(__dirname + "/public", express.static());
 
 
 
